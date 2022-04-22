9a70b947237efd1590767a47bcea81f4ffcd0221
givamarques <49195218+givamarques@users.noreply.github.com>
2022-04-22 16:50:56 -0300

    aula01 variaveis

---
+++ aula01variaveis.dart
@@ -0,0 +1,16 @@
+void main() {
+  for (var i = 1; i < 6; i++) {
+    print('hello $i');
+  }
+
+  int idade = 30;
+  String nome = "Giva";
+  double distancia = 24.89;
+
+  print(idade);
+  print(nome);
+
+  idade = 36;
+  //o valor da variavel sera atualizado com a ultima informacao passada
+  print(idade);
+}
