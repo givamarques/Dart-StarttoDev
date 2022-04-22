3d639ceb28e3019cd8b4631bfb2b8a41436c90cf
givamarques <49195218+givamarques@users.noreply.github.com>
2022-04-22 18:26:48 -0300

    exercicio 01 variaveis

---
+++ exercicio01variaveis.dart
@@ -0,0 +1,28 @@
+void main() {
+  /*armazene valores dentro das variaveis
+nome, idade, rg e altura
+exiba os valores
+
+diga o tipo de variavel
+
+texto =String
+numeros = inteiros = int
+numeros quebrados = double
+
+Estrutura das variaveis
+
+tipo nome = valor
+
+utilize o comando print()
+ */
+
+  String nome = "Giva";
+  int idade = 36;
+  String rg = "1357942";
+  double altura = 1.73;
+
+  print(nome);
+  print(rg);
+  print(idade);
+  print(altura);
+}
