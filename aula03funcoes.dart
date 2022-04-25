e727f1a99edb0fd69b5d3193a19599cc98b19f22
givamarques <49195218+givamarques@users.noreply.github.com>
2022-04-25 01:24:26 -0300

    Add files via upload

--- aula03funcoes.dart
+++ aula03funcoes.dart
@@ -1,13 +1,20 @@
 void main() {
-  saudacoes();
+  //aqui o nome esta sendo enviado para a funcao
+  saudacoes('Giva');
 }
 
-void saudacoes() {
-  print('Olá!');
+//passando o parametro string nome
+void saudacoes(String nome) {
+  //aqui sendo usado o $ antes da variavel vai puxar a inf de saudacoes dentro da main
+  print('Olá! $nome');
+  //para usar $ com funcao precisa passar chaves e funcao segue com ()
   print('Agora: ${agora()}');
 }
 
+//nao pode ser void pois tem retorno
 String agora() {
+  //a variavel agora vai exibir o tempo atual
   DateTime agora = DateTime.now();
+  //vai retornar o tempo da variavel agora como string
   return agora.toString();
 }
